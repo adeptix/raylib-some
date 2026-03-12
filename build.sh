841c655dffa32f4c613e430533a9862fbd7ca3e4
adeptix <adeptgoo@gmail.com>
2026-03-12 21:08:05 +0400

    ci: update go version in workflow

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 MAIN_GO_FILE="wasm-main.go"
-INDEX_DIR="index"
+OUT_DIR="_site"
 WASM_LIB_DIR="Raylib-Go-Wasm"
 
-set -x
+set -e
 
 if [ ! -e $MAIN_GO_FILE ]; then
     echo "error: $WASM_LIB_DIR is required"
@@ -22,16 +22,16 @@ echo "require github.com/BrownNPC/wasm-ffi-go v1.2.0 // indirect" >> go.mod
 
 go mod download
 
-mkdir -p $INDEX_DIR/rl
+mkdir -p $OUT_DIR/rl
 
-cp $WASM_LIB_DIR/index/rl/raylib.js   $INDEX_DIR/rl/
-cp $WASM_LIB_DIR/index/rl/raylib.wasm $INDEX_DIR/rl/
-cp $WASM_LIB_DIR/index/index.html     $INDEX_DIR/
-cp $WASM_LIB_DIR/index/index.js       $INDEX_DIR/
-cp $WASM_LIB_DIR/index/wasm_exec.js   $INDEX_DIR/
+cp $WASM_LIB_DIR/index/rl/raylib.js   $OUT_DIR/rl/
+cp $WASM_LIB_DIR/index/rl/raylib.wasm $OUT_DIR/rl/
+cp $WASM_LIB_DIR/index/index.html     $OUT_DIR/
+cp $WASM_LIB_DIR/index/index.js       $OUT_DIR/
+cp $WASM_LIB_DIR/index/wasm_exec.js   $OUT_DIR/
 
 echo "compile..."
-GOOS=js GOARCH=wasm go build -o $INDEX_DIR/main.wasm wasm-main.go
+GOOS=js GOARCH=wasm go build -o $OUT_DIR/main.wasm wasm-main.go
 
 rm go.mod
 mv temp_go.mod go.mod
